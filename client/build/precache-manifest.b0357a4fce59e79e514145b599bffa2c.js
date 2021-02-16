7350ceaa6ce19a1bdf23a8e6387037245e26e310
Sammye Jones <sajones@ucdavis.edu>
2021-02-16 12:57:04 -0800

    ran build in client

---
+++ client/build/precache-manifest.b0357a4fce59e79e514145b599bffa2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9273065e2a68f9278628bc59501b9e41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbcb7167ee8f8bbb7158",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "ed8dd2ee32d6955a3eb3",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "fbcb7167ee8f8bbb7158",
+    "url": "/static/js/2.81407ff9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.81407ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8dd2ee32d6955a3eb3",
+    "url": "/static/js/main.faa1fd3e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
