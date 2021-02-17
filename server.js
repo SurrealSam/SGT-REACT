9ceaa5fbbff2c1eab140207ccbd6b012be74b7c0
Sammye Jones <sajones@ucdavis.edu>
2021-02-17 09:14:29 -0800

    removed if clause on server

--- server.js
+++ server.js
@@ -160,14 +160,14 @@ app.get("/movements", (req, res) => {
 //----------------------------------------- END OF ROUTES---------------------------------------------------
 //Start Server
 
-if (process.env.NODE_ENV === 'production') {
+
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
-}
+
 
 const PORT = process.env.PORT || 4000;
 
